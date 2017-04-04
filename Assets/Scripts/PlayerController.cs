7663544144975d7c2432cbf991004cc7b0bfb8c7
Jonas Kårström <jonas.karstrom@mejsla.se>
2017-04-04 21:11:53 +0200

    Initial commit

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour {
+
+	private Rigidbody rb;
+
+	public float speed = 0;
+
+	void Start () {
+		rb = GetComponent<Rigidbody> ();
+	}
+	
+	void FixedUpdate () {
+		float verticalForce = Input.GetAxis ("Vertical");
+		float horizontalForce = Input.GetAxis ("Horizontal");
+
+		Vector3 vectorForce = new Vector3 (horizontalForce, 0, verticalForce);
+
+		rb.AddForce (vectorForce * speed);
+	}
+}
